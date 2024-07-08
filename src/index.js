02b1a5f5f79dd939c089d24b58f3569da5359717
Anastasia Konushok <nastjenka.kon@gmail.com>
2024-07-08 14:25:13 +0300

    feat: resolve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number === 0) return units[0];
+
+    let readable = '';
+
+    if (number >= 100) {
+        let hundreds = Math.floor(number / 100);
+        readable += units[hundreds] + ' hundred';
+        number %= 100;
+        if (number > 0) {
+            readable += ' ';
+        }
+    }
+
+    if (number >= 20) {
+        let ten = Math.floor(number / 10);
+        readable += tens[ten];
+        number %= 10;
+        if (number > 0) {
+            readable += ' ';
+        }
+    }
+
+    if (number >= 10 && number < 20) {
+        readable += teens[number - 10];
+    } else if (number > 0 && number < 10) {
+        readable += units[number];
+    }
+
+    return readable.trim();
 }
